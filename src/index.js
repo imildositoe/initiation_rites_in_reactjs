bb1943a782025435b4f56ad462f603594040d6ed
imildositoe <imildositoe@gmail.com>
2021-07-07 23:11:56 +0200

    color added as a props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", null, "Hello World!"), //what we want to create
-  document.getElementById('root') // where we want to create
+    React.createElement(
+        "h1",
+        {style: {color: "blue"}},
+        "Hello World!"
+    ), //what we want to create
+    document.getElementById('root') // where we want to create
 );
