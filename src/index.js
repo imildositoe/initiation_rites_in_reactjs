1bdb674e3c01ec8301f2e5b5937d722837c3ffe0
imildositoe <imildositoe@gmail.com>
2021-07-07 22:57:21 +0200

    Hello world displayed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  React.createElement("h1", null, "Hello World!"), //what we want to create
+  document.getElementById('root') // where we want to create
 );
