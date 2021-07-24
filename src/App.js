ec83702ef887a63be033b6b16c888e84c40a4031
imildositoe <imildositoe@gmail.com>
2021-07-24 23:43:43 +0200

    table structure mounted

--- src/App.js
+++ src/App.js
@@ -39,8 +39,19 @@ function Table() {
     return (
         <table>
             <thead>
-                <th></th>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
             </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
         </table>
     );
 }
